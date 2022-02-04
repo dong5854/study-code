3930fcb295db597de32be0adb9b81f0c59f19bda
이동영 <dong5854@gmail.com>
2022-02-04 18:44:21 +0900

    first commit

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+
+$ip = $_SERVER['REMOTE_ADDR'];
+$referer = $_SERVER['HTTP_REFERER'];
+
+$uri = $_SERVER['REQUEST_URI'];
+$request = parse_url($uri, PHP_URL_PATH);
+$params = parse_url($uri, PHP_URL_QUERY);
+
+switch($request){
+    case '/':
+    case "":
+        require __DIR__.'/main.html';
+        break;
+    default:
+        require __DIR__.'/main.html';
+    break;
+}
+
+?>
