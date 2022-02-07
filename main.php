c01944457fc3bb6c191a11d343f94645ce8d80ac
이동영 <dong5854@gmail.com>
2022-02-07 15:10:24 +0900

    test: datatable form input

--- main.php
+++ main.php
@@ -4,27 +4,18 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.11.4/css/jquery.dataTables.css">
     <script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
+    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.11.4/css/jquery.dataTables.min.css">
+    <style>
+        <?php require_once("./css/datatable.css") ?>
+    </style>
     <title>main</title>
 </head>
 <body>
-    <table id="my_datatable" class="display">
-        <thead>
-            <tr>
-                <th>type</th>
-                <th>status</th>
-            </tr>
-        </thead>
-    </table>
+    <?php include "./components/inputdataTable.html"; ?>
 </body>
 <script>
-$(document).ready( function () {
-    $('#my_datatable').DataTable();
-} );
-</script>
-<script>
-    <?php require_once("./js/datatables.js") ?>
+    <?php require_once("./js/forminput.js") ?>
 </script>
 <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.11.4/js/jquery.dataTables.js"></script>
 </html>
