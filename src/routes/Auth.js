f202b4e1945f43c9211f030b98d9a13ae59c4bd3
DongYoung <dong5854@gmail.com>
2022-06-13 03:23:05 +0900

    Authentacation: Email, google, github

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,8 +1,18 @@
 import { useState } from 'react';
+import { authService } from 'fbase';
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  GoogleAuthProvider,
+  GithubAuthProvider,
+  signInWithPopup,
+} from 'firebase/auth';
 
 const Auth = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [newAccount, setNewAccount] = useState(true);
+  const [error, setError] = useState('');
   const onChange = (event) => {
     const {
       target: { name, value },
@@ -13,8 +23,38 @@ const Auth = () => {
       setPassword(value);
     }
   };
-  const onSubmit = (event) => {
+  const onSubmit = async (event) => {
     event.preventDefault();
+    try {
+      let data;
+      if (newAccount) {
+        data = await createUserWithEmailAndPassword(
+          authService,
+          email,
+          password,
+        );
+        console.log(data);
+      } else {
+        data = await signInWithEmailAndPassword(authService, email, password);
+      }
+      console.log(data);
+    } catch (error) {
+      setError(error.message);
+    }
+  };
+  const toggleAccount = () => setNewAccount((prev) => !prev);
+  const onSocialClick = async (event) => {
+    const {
+      target: { name },
+    } = event;
+    let provider;
+    if (name === 'google') {
+      provider = new GoogleAuthProvider();
+    } else if (name === 'github') {
+      provider = new GithubAuthProvider();
+    }
+    const result = await signInWithPopup(authService, provider);
+    console.log(result);
   };
   return (
     <div>
@@ -35,11 +75,22 @@ const Auth = () => {
           value={password}
           onChange={onChange}
         />
-        <input type="submit" value="Log in" />
+        <input
+          type="submit"
+          value={newAccount ? 'Create Account' : 'Sign In'}
+        />
+        {error}
       </form>
+      <span onClick={toggleAccount}>
+        {newAccount ? 'Sign In' : 'create Account'}
+      </span>
       <div>
-        <button>Continue with Google</button>
-        <button>Continue with Github</button>
+        <button onClick={onSocialClick} name="google">
+          Continue with Google
+        </button>
+        <button onClick={onSocialClick} name="github">
+          Continue with Github
+        </button>
       </div>
     </div>
   );
--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,1 +1,16 @@
-export default () => <span>Profile</span>;
+import { authService } from 'fbase';
+import { useNavigate } from 'react-router-dom';
+import { signOut } from 'firebase/auth';
+
+export default () => {
+  const Navigate = useNavigate();
+  const onLogOutClick = () => {
+    signOut(authService);
+    Navigate('/');
+  };
+  return (
+    <>
+      <button onClick={onLogOutClick}>Sign Out</button>
+    </>
+  );
+};
