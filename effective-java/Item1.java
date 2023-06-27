6117af6db86a5b5f15260f4448f51a26d101fead
dong young <dong5854@gmail.com>
2023-06-28 05:56:10 +0900

    EFFECTIVE JAVA 3판 - 아이템 1, 생성자 대신 정적 팩터리 메서드를 고려하라

---
+++ effective-java/Item1.java
@@ -0,0 +1,12 @@
+import java.util.*;
+import java.math.*;
+
+public class Item1 {
+    public static void main(String[] args) {
+        BigInteger bi = BigInteger.probablePrime(3, new Random());
+        System.out.println(bi);
+        bi = new BigInteger(3,5,new Random());
+        System.out.println(bi);
+        Boolean.valueOf(true);
+    }
+}
