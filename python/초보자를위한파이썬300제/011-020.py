bb58ccf364291e2b0ca805457939d25a5979e9f3
dong young <dong5854@gmail.com>
2023-12-24 22:34:18 +0900

    초보자를 위한 파이썬 300제 11~20

---
+++ python/초보자를위한파이썬300제/011-020.py
@@ -0,0 +1,52 @@
+# 011
+삼성전자 = 50000
+총평가금액 = 삼성전자 * 10
+print(총평가금액)
+
+# 012
+시가총액 = 298000000000000
+현재가 = 50000
+PER = 15.79
+print(시가총액, type(시가총액))
+print(현재가, type(현재가))
+print(PER, type(PER))
+
+# 013
+s = "hello"
+t = "python"
+print(s+"!"+t)
+
+# 014
+print(2+2*3)
+
+# 015
+a = "132"
+print(type(a))
+
+# 016
+num_str = "720"
+print(type(num_str))
+num_str = int(num_str)
+print(type(num_str))
+
+# 017
+num = 100
+result = str(num)
+print(result, type(result))
+
+# 018
+data = "15.79"
+print(type(data))
+data = float(data)
+print(data, type(data))
+
+# 019
+year = "2020"
+print(int(year)-3)
+print(int(year)-2)
+print(int(year)-1)
+
+# 020
+월 = 48584
+총금액 = 월 * 36
+print(총금액)
